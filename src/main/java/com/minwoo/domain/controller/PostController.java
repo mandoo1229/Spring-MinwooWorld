4038b27a21540e3c3d05f739be26d8dbd021dee6
mandoo1229 <skaalsdn35@gmail.com>
2024-03-13 09:36:42 +0900

    Feat : 글쓰기 페이지 컨트롤러 작성

---
+++ src/main/java/com/minwoo/domain/controller/PostController.java
@@ -0,0 +1,25 @@
+package com.minwoo.domain.controller;
+
+import com.minwoo.domain.post.PostRequest;
+import com.minwoo.domain.post.PostResponse;
+import com.minwoo.domain.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequiredArgsConstructor
+public class PostController {
+    private final PostService postService;
+
+    @GetMapping("/post/write.do")
+    public String openPostWrite(@RequestParam(value = "id", required = false) final Long id, Model model) {
+        if (id != null) {
+            PostResponse post = postService.findPostById(id);
+            model.addAttribute("post", post);
+        }
+        return "post/write";
+    }
+}
