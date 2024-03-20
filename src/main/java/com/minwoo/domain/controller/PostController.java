ad7ac3847434665cc0399d0f6c0df73cb056a001
mandoo1229 <skaalsdn35@gmail.com>
2024-03-20 10:55:35 +0900

    Feat : 게시글 수정하기 기능 추가

--- src/main/java/com/minwoo/domain/controller/PostController.java
+++ src/main/java/com/minwoo/domain/controller/PostController.java
@@ -45,4 +45,13 @@ public class PostController {
         model.addAttribute("post", post);
         return "post/view";
     }
+
+    // 기존 게시글 수정
+    @PostMapping("/post/update.do")
+    public String updatePost(final PostRequest params) {
+        postService.updatePost(params);
+        return "redirect:/post/list.do";
+    }
 }
+
+
