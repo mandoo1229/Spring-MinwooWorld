2cd337d53ced163c3b7000298f360900d10e9c8b
mandoo1229 <skaalsdn35@gmail.com>
2024-03-21 16:18:10 +0900

    Feat : 게시물 100개 등록

--- src/test/java/com/minwoo/MinwooworldApplicationTests.java
+++ src/test/java/com/minwoo/MinwooworldApplicationTests.java
@@ -16,58 +16,5 @@ import java.util.List;
 
 @SpringBootTest
 class MinwooworldApplicationTests {
-	@Autowired
-	PostMapper postMapper;
-
-	@Test
-	void save() {
-		PostRequest params = new PostRequest();
-		params.setTitle("1번 게시글");
-		params.setContent("1번 게시글 내용");
-		params.setWriter("테스터");
-		params.setNoticeYn(false);
-		postMapper.save(params);
-
-		List<PostResponse> posts = postMapper.findAll();
-		System.out.println("전체 게시글 수 : " + posts.size() + "개입니다.");
-	}
-
-	@Test
-	void findById() {
-		PostResponse post = postMapper.findById(1L);
-		try {
-			String postJson = new ObjectMapper().registerModule(new JavaTimeModule()).writeValueAsString(post);
-			System.out.println(postJson);
-
-		} catch (JsonProcessingException e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-	@Test
-	void update() {
-		PostRequest params = new PostRequest();
-		params.setId(1L);
-		params.setTitle("1번 게시글 제목 수정");
-		params.setContent("1번게시글 내용 수정");
-		params.setWriter("미누");
-		params.setNoticeYn(true);
-		postMapper.update(params);
-
-		PostResponse post = postMapper.findById(1L);
-		try {
-			String postJson = new ObjectMapper().registerModule(new JavaTimeModule()).writeValueAsString(post);
-			System.out.println(postJson);
-		} catch (JsonProcessingException e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-	@Test
-	void delete() {
-		System.out.println("삭제 전 : " + postMapper.findAll().size());
-		postMapper.deleteById(1L);
-		System.out.println("삭제 후 : " + postMapper.findAll().size());
-	}
 
 }
--- src/test/java/com/minwoo/PostServiceTest.java
+++ src/test/java/com/minwoo/PostServiceTest.java
@@ -10,15 +10,15 @@ import org.springframework.boot.test.context.SpringBootTest;
 public class PostServiceTest {
     @Autowired
     PostService postService;
-
     @Test
-    void save() {
-        PostRequest params = new PostRequest();
-        params.setTitle("1번 게시글 제목");
-        params.setContent("1번 게시글 내용");
-        params.setWriter("민우");
-        params.setNoticeYn(false);
-        Long id = postService.savePost(params);
-        System.out.println("생성된 게시글 ID : " + id);
+    void saveByForeach() {
+        for (int i = 1; i < 1000; i++) {
+            PostRequest params = new PostRequest();
+            params.setTitle(i + "번 게시글 제목");
+            params.setContent(i + "번 게시글 내용");
+            params.setWriter("테스트" + i);
+            params.setNoticeYn(false);
+            postService.savePost(params);
+        }
     }
 }
